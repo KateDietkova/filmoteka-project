c84da3a9423178f571b4d0bbb68b35c48a50fda9
Yulika Zagorovsky <ju.zagorovsky@gmail.com>
2022-11-05 23:05:42 +0100

    adds js to modal-film and hbs temp

---
+++ src/js/modalFilmMarkup.js
@@ -0,0 +1,78 @@
+import modalFilmMarkupTpl from '../templates/modalFilmMarkup.hbs';
+
+const refs = {
+  //   galleryEl: document.querySelector('.gallery'),
+  modal: document.querySelector('[data-modal]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  modalContainer: document.querySelector('.modal-container'),
+};
+
+// refs.galleryEl.addEventListener('click', onClickShowModal);
+function onClickShowModal(event) {
+  event.preventDefault();
+  // if (!event.target.classList.contains('имя-класса-карточки') ) {
+  //     //прописать условие, когда кликнули не туда
+  //     return;
+  //   };
+  showModal(event);
+}
+
+function showModal(event) {
+  // сохранить данные из карточки в объект
+  // const { src, title, vote, votes, и т.д. } = event.target;
+  const dataObj = {};
+  // отобразить модалку
+  toggleModalClass();
+  // отрисовать разметку модалки
+  refs.modalContainer.insertAdjacentHTML(
+    'afterbegin',
+    modalFilmMarkupTpl(dataObj)
+  );
+  // навесить слушателей на закрытие
+  addListeners();
+}
+
+function onBtnClick(event) {
+  event.preventDefault();
+  toggleModalClass();
+  removeListeners();
+}
+
+function onKeyDown(event) {
+  event.preventDefault();
+  if (event.key === 'Escape') {
+    toggleModalClass();
+    removeListeners();
+  }
+}
+
+function onBackdropClick(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    toggleModalClass();
+    removeListeners();
+  }
+}
+
+// Навешивает и убирает класс is-hidden
+function toggleModalClass() {
+  refs.modal.classList.toggle('is-hidden');
+}
+
+// Навешивает слушателей на закрытие
+function addListeners() {
+  if (!refs.modal.classList.contains('is-hidden')) {
+    refs.closeModalBtn.addEventListener('click', onBtnClick);
+    window.addEventListener('keydown', onKeyDown);
+    refs.modal.addEventListener('click', onBackdropClick);
+  }
+}
+
+// Снимает слушателей на закрытие
+function removeListeners() {
+  if (refs.modal.classList.contains('is-hidden')) {
+    refs.closeModalBtn.removeEventListener('click', onBtnClick);
+    window.removeEventListener('keydown', onKeyDown);
+    refs.modal.removeEventListener('click', onBackdropClick);
+  }
+}
