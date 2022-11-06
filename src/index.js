260414924bbe61a83e8556c70928dde57742de9c
Pavel Mogilevskyi <pavel13595@gmail.com>
2022-11-06 18:05:24 +0200

    feature/modal-team and loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-
 import './js/getTrendFilm';
 import './js/getGenres';
 import './js/getFilmByKeywords';
 import './js/galleryMarkup';
 import './js/modalFilmMarkup';
+import './js/modal-team/modal-team';
