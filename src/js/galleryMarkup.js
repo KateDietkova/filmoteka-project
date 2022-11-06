0ccbc955664216a337cb9b3c239c159d3b32dc91
Katerina Dietkova <katrinkir1296@gmail.com>
2022-11-06 18:31:02 +0200

    update code popularfilm js

--- src/js/galleryMarkup.js
+++ src/js/galleryMarkup.js
@@ -1,25 +1,15 @@
+import { getMovie } from './getTrendFilm';
+import { getAllGenres, getGenres } from './getGenres';
+
 const gallery = document.querySelector('.movie-list');
-// let genre;
 
-async function getMovie() {
-  const movies = await fetch(
-    'https://api.themoviedb.org/3/trending/movie/week?api_key=579a7483bae7d6a5a25eb4c1ddded2cf&page=1'
-  );
-  const resp = await movies.json();
-  console.log(resp.results);
-  return resp.results;
-}
-
-async function render() {
+async function getMoviesWithAllGenres() {
   const movies = await getMovie();
   const allGenres = await getAllGenres();
   return { movies, allGenres };
 }
-render().then(({ movies, allGenres }) => {
-  console.log(allGenres);
-  // galleryMarkup(movies, allGenres);
-  gallery.innerHTML = galleryMarkup(movies, allGenres);
-});
+
+addMoviesToGallery();
 
 function galleryMarkup(movies, allGenres) {
   return movies
@@ -47,33 +37,12 @@ function galleryMarkup(movies, allGenres) {
     .join('');
 }
 
-function getGenres(genresMovie, allGenres) {
-  console.log(allGenres);
-  const filterGenres = allGenres.filter(genre =>
-    genresMovie.includes(genre.id)
-  );
-  const genresName = filterGenres.map(genre => {
-    return genre.name;
-  });
-  if (genresName.length > 2) {
-    return `${genresName.slice(0, 2).join(', ')}, Other`;
-  }
-  console.log(genresName);
-
-  return genresName.join(', ');
-}
-
 function getDate(date) {
   const year = date.split('-')[0];
   return year;
 }
 
-async function getAllGenres() {
-  const genresResponse = await fetch(
-    'https://api.themoviedb.org/3/genre/movie/list?api_key=579a7483bae7d6a5a25eb4c1ddded2cf'
-  );
-  const genresParse = await genresResponse.json();
-  const allGenres = genresParse.genres;
-  console.log(allGenres);
-  return allGenres;
+async function addMoviesToGallery() {
+  const { movies, allGenres } = await getMoviesWithAllGenres();
+  gallery.innerHTML = galleryMarkup(movies, allGenres);
 }
