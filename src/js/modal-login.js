b887b0063b417fe368ae0095969fb6abd4ce8e04
AntonMaydan <92736982+AntonMaydan@users.noreply.github.com>
2022-11-08 19:53:37 +0200

    modalIn

--- src/js/modal-login.js
+++ src/js/modal-login.js
@@ -4,10 +4,19 @@ const openModalBtn = document.querySelector('[data-modal-open]');
 console.log(openModalBtn);
 const modal = document.querySelector('[data-loginmodal]');
 console.log(modal);
+const modalForm = document.querySelector('.modal__form');
+
 
 openModalBtn.addEventListener('click', toggleModal);
 closeModalBtn.addEventListener('click', toggleModal);
+modalForm.addEventListener('submit', onFormSubmit);
+
 
 function toggleModal() {
   modal.classList.toggle('is-hidden');
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
 }
