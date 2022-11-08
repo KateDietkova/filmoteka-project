33723b393fb74fa8b41b746c9c0738af12a75fe1
Katerina Dietkova <katrinkir1296@gmail.com>
2022-11-08 19:50:36 +0200

    Fixed img No find movie

--- src/js/fetchFunction.js
+++ src/js/fetchFunction.js
@@ -1,29 +1,33 @@
-import axios from "axios";
-import Notiflix from "notiflix";
-import { getFilmByKeywords } from "./fetchFunction";
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import { getFilmByKeywords } from './fetchFunction';
 
-export async function getFilmByKeywords(queryVal, pageNum){
-    
-    const url = `https://api.themoviedb.org/3/search/movie/`;
-    
-    return await axios
-      .get(url, {
-        params: {
-          api_key: '579a7483bae7d6a5a25eb4c1ddded2cf',
-          query: `${queryVal}`,
-          page: `${pageNum}`,
-        },
-      })
-      .then(res => {
-        if (!res.data.total_results) {
-          throw new Error();
-        }
+const movieErrorWrapper = document.querySelector('.movie-error-wrapper');
 
+export async function getFilmByKeywords(queryVal, pageNum) {
+  const url = `https://api.themoviedb.org/3/search/movie/`;
+
+  return await axios
+    .get(url, {
+      params: {
+        api_key: '579a7483bae7d6a5a25eb4c1ddded2cf',
+        query: `${queryVal}`,
+        page: `${pageNum}`,
+      },
+    })
+    .then(res => {
+      if (!res.data.total_results) {
+        throw new Error();
+      }
+      if (!movieErrorWrapper.classList.contains('visually-hidden')) {
+        movieErrorWrapper.classList.add('visually-hidden');
+      }
         return res.data.results;
-      }).catch(error => {
-          Notiflix.Notify.failure(
-            'Sorry, there are no movies matching your search query. Please try again.'
-          );
-      });
-};
-
+    })
+    .catch(error => {
+      movieErrorWrapper.classList.remove('visually-hidden');
+      Notiflix.Notify.failure(
+        'Sorry, there are no movies matching your search query. Please try again.'
+      );
+    });
+}
