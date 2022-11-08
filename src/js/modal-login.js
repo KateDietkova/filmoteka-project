c3c6da64ab7490e7de6a4b7667781125050fc28f
YuliiaMakhovska <y.makhovska@gmail.com>
2022-11-08 20:57:57 +0200

    fix modal & add button

--- src/js/modal-login.js
+++ src/js/modal-login.js
@@ -1,13 +1,13 @@
-const closeModalBtn = document.querySelector('[data-loginmodal-close]');
-console.log(closeModalBtn);
-const openModalBtn = document.querySelector('[data-modal-open]');
-console.log(openModalBtn);
-const modal = document.querySelector('[data-loginmodal]');
-console.log(modal);
+try {
+  const closeModalBtn = document.querySelector('[data-loginmodal-close]');
+  const openModalBtn = document.querySelector('[data-modal-open]');
+  const modal = document.querySelector('[data-loginmodal]');
 
-openModalBtn.addEventListener('click', toggleModal);
-closeModalBtn.addEventListener('click', toggleModal);
-
-function toggleModal() {
-  modal.classList.toggle('is-hidden');
+  openModalBtn.addEventListener('click', toggleModal);
+  closeModalBtn.addEventListener('click', toggleModal);
+  function toggleModal() {
+    modal.classList.toggle('is-hidden');
+  }
+} catch (error) {
+  return;
 }
