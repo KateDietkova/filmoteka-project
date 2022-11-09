3d0ee101a04acf2c64f7a31178ff2f3ebfcfd65c
Katerina Dietkova <katrinkir1296@gmail.com>
2022-11-09 22:17:59 +0200

    add styles to pagination

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -9,19 +9,19 @@ export let options = {
   firstItemClassName: 'tui-first-child',
   lastItemClassName: 'tui-last-child',
   template: {
-    page: '<a href="#" class="tui-page-btn">{{page}}p</a>',
+    page: '<a href="#" class="tui-page-btn page-pagination">{{page}}</a>',
     currentPage:
-      '<strong class="tui-page-btn tui-is-selected">{{page}}p</strong>',
+      '<strong class="tui-page-btn tui-is-selected selected-page">{{page}}</strong>',
     moveButton:
-      '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}} page-pagination move-btn">' +
+      '<span class="tui-ico-{{type}} move-btn-arrow"></span>' +
       '</a>',
     disabledMoveButton:
-      '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">' +
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}} page-pagination">' +
       '<span class="tui-ico-{{type}}">{{type}}</span>' +
       '</span>',
     moreButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}">' +
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}} page-pagination">' +
       '<span class="tui-ico-ellip">...</span>' +
       '</a>',
   },
