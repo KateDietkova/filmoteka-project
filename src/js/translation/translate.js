1d361485082060dc250e71a5b158c07767dd76e6
Yulika Zagorovsky <ju.zagorovsky@gmail.com>
2022-11-10 15:20:03 +0100

    fixes function

--- src/js/translation/translate.js
+++ src/js/translation/translate.js
@@ -2,41 +2,46 @@ import { translations } from './langs';
 
 let lang = '';
 
-window.addEventListener('load', checkStorage);
+window.addEventListener('load', onLoad);
 
 const select = document.getElementById('langs');
 select.addEventListener('change', onChangeTranslate);
 
-function checkStorage() {
+function onLoad() {
+  getLangFromStorage();
+  changePathname();
+  translateTexts();
+}
+
+function getLangFromStorage() {
   if (localStorage.getItem('lang')) {
     lang = localStorage.getItem('lang');
-    console.log(typeof lang); // проверить тип данных на выходе из стореджа
     select.value = lang;
-    changePathname();
-    translateTexts();
+  } else {
+    lang = select.value;
   }
 }
 
 function onChangeTranslate() {
   updateStorage();
   changePathname();
-  // В фетч импортнуть lang из хранилища для запроса с текущим языком
-  // добавить сюда асинхронный фетч с новым запросом
   translateTexts();
+  translatePlaceholder();
 }
 
-// В рендер модалки импортнуть эту функцию, translations и lang
-export function translateTexts() {
+function translateTexts() {
   for (let key in translations) {
     let elem = document.querySelector('.lng-' + key);
     if (elem) {
       elem.innerHTML = translations[key][lang];
     }
   }
+}
+
+function translatePlaceholder() {
+  console.log(document.querySelector('.header-input'));
   const headerInput = document.querySelector('.header-input');
-  if (headerInput) {
-    headerInput.placeholder = translations.search[lang];
-  }
+  headerInput.placeholder = translations.search[lang];
 }
 
 function updateStorage() {
@@ -47,3 +52,5 @@ function updateStorage() {
 function changePathname() {
   location.href = `${window.location.pathname}#${lang}`;
 }
+
+export { lang, translateTexts, getLangFromStorage };
