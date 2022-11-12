0def4fcd2f8311c1e4506e80ecb5a244fb489e2c
YuliiaMakhovska <y.makhovska@gmail.com>
2022-11-12 19:10:00 +0200

    add background image

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,4 +1,5 @@
 const darkThemeBtn = document.querySelector('.toggle-darktheme-btn');
+const header = document.querySelector('.header');
 
 darkThemeBtn.addEventListener('click', () => {
   if (document.body.classList.contains('darkTheme')) {
@@ -9,16 +10,19 @@ darkThemeBtn.addEventListener('click', () => {
 });
 
 function onLigthTheme() {
+  header.classList.remove('headerDark');
   document.body.classList.remove('darkTheme');
   darkThemeBtn.textContent = '🌙';
   localStorage.theme = 'ligth';
 }
 
 function onDarkTheme() {
+  header.classList.add('headerDark');
   document.body.classList.add('darkTheme');
   darkThemeBtn.textContent = '🔆';
   localStorage.theme = 'darkTheme';
 }
+
 if (localStorage.theme === 'darkTheme') {
   onDarkTheme();
 }
