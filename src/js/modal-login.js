0556766b9191754ba5ab2752ae4ca9390a2720c0
ValeriiaM23 <mezencevavalerie@gmail.com>
2022-11-13 13:42:19 +0100

    html and css modal signup

--- src/js/modal-login.js
+++ src/js/modal-login.js
@@ -1,6 +1,9 @@
 const openModalBtn = document.querySelector('[data-modal-open]');
 const modal = document.querySelector('[data-loginmodal]');
 const closeModalBtn = document.querySelector('[data-loginmodal-close]');
+const signUpBtn = document.querySelector('[data-signupmodal]');
+const signUpModal = document.querySelector('.signup-modal');
+
 
 if (openModalBtn) {
   openModalBtn.addEventListener('click', openModal);
@@ -12,8 +15,13 @@ if (closeModalBtn) {
 
 function openModal() {
   modal.classList.remove('is-hidden');
+  signUpBtn.addEventListener('click', openSignUp)
 }
 
 function closeModal() {
   modal.classList.add('is-hidden');
+}
+
+function openSignUp() {
+  signUpModal.classList.remove('is-hidden');
 }
