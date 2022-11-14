2bb6a95c92c1b3ef98c038beffcd9b5241d0aa95
Katerina Dietkova <katrinkir1296@gmail.com>
2022-11-14 18:22:31 +0200

    add styles to modal login

--- src/js/modal-login.js
+++ src/js/modal-login.js
@@ -1,10 +1,6 @@
 const openModalBtn = document.querySelector('[data-modal-open]');
-const closeModalBtn = document.querySelector('[data-loginmodal-close]');
-const closeSignUpBtn = document.querySelector('[data-signupmodal-close]');
 const modal = document.querySelector('[data-loginmodal]');
-const signUpModal = document.querySelector("[data-signupmodal]");
-const signUpBtn = document.querySelector('#sign-up-btn');
-const signInBtn = document.querySelector('#sign-in-btn');
+const closeModalBtn = document.querySelector('[data-loginmodal-close]');
 
 if (openModalBtn) {
   openModalBtn.addEventListener('click', openModal);
@@ -12,27 +8,12 @@ if (openModalBtn) {
 
 if (closeModalBtn) {
   closeModalBtn.addEventListener('click', closeModal);
-  closeSignUpBtn.addEventListener('click', closeModal);
-} 
+}
 
 function openModal() {
   modal.classList.remove('is-hidden');
-  signUpModal.classList.add('is-hidden');
-  if (signUpBtn) {
-    signUpBtn.addEventListener('click', openSignUpModal)
-  }
 }
 
 function closeModal() {
   modal.classList.add('is-hidden');
-  signUpModal.classList.add('is-hidden');
-}
-
-
-function openSignUpModal() {
-signUpModal.classList.remove('is-hidden');
-modal.classList.add('is-hidden');
-  if (signInBtn) {
-    signInBtn.addEventListener('click', openModal);
-  }
 }
