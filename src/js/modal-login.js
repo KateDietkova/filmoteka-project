5e236045989a417fa21b055c94674a7ca5f044d3
ValeriiaM23 <mezencevavalerie@gmail.com>
2022-11-14 12:28:01 +0100

    login modal

--- src/js/modal-login.js
+++ src/js/modal-login.js
@@ -1,19 +1,44 @@
-const openModalBtn = document.querySelector('[data-modal-open]');
-const modal = document.querySelector('[data-loginmodal]');
-const closeModalBtn = document.querySelector('[data-loginmodal-close]');
+const refs = {
+  openModalBtn:document.querySelector('[data-modal-open]'),
+  formSignIn: document.querySelector('#form-sign-in'),
+  formSignUp: document.querySelector('#form-sign-up'),
+  logInBtn: document.querySelector('#log-in-btn'),
+  signInBtn: document.querySelector('#sign-in-btn'),
+  signUpBtn: document.querySelector('#sign-up-btn'),
+  closeModalBtn: document.querySelector('[data-loginmodal-close]'),
+  closeSignUpBtn: document.querySelector('[data-signupmodal-close]'),
+  modal: document.querySelector('[data-loginmodal]'),
+  signUpModal: document.querySelector("[data-signupmodal]"),
 
-if (openModalBtn) {
-  openModalBtn.addEventListener('click', openModal);
 }
 
-if (closeModalBtn) {
-  closeModalBtn.addEventListener('click', closeModal);
+if (refs.openModalBtn) {
+  refs.openModalBtn.addEventListener('click', openModal);
 }
+
+if (refs.closeModalBtn) {
+  refs.closeModalBtn.addEventListener('click', closeModal);
+  refs.closeSignUpBtn.addEventListener('click', closeModal);
+} 
 
 function openModal() {
-  modal.classList.remove('is-hidden');
+  refs.modal.classList.remove('is-hidden');
+  refs.signUpModal.classList.add('is-hidden');
+  if (refs.signUpBtn) {
+    refs.signUpBtn.addEventListener('click', openSignUpModal)
+  }
 }
 
 function closeModal() {
-  modal.classList.add('is-hidden');
+  refs.modal.classList.add('is-hidden');
+  refs.signUpModal.classList.add('is-hidden');
+}
+
+
+function openSignUpModal() {
+refs.signUpModal.classList.remove('is-hidden');
+refs.modal.classList.add('is-hidden');
+  if (refs.signInBtn) {
+    refs.signInBtn.addEventListener('click', openModal);
+  }
 }
