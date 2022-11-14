d56a44e9d69cc2907f46624f4d5c486a4b1624a0
Katerina Dietkova <katrinkir1296@gmail.com>
2022-11-14 13:45:18 +0200

    fixed offset in AOS

--- src/js/smoothAppearanceFilmCard.js
+++ src/js/smoothAppearanceFilmCard.js
@@ -13,7 +13,7 @@ AOS.init({
   throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
 
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 120, // offset (in px) from the original trigger point
+  offset: 90, // offset (in px) from the original trigger point
   delay: 0, // values from 0 to 3000, with step 50ms
   duration: 900, // values from 0 to 3000, with step 50ms
   easing: 'ease-in-out', // default easing for AOS animations
